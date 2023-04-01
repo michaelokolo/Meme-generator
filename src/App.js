f49e1659e4549af5767d24c9047c291972a7524c
michaelokolo <91018269+michaelokolo@users.noreply.github.com>
2023-04-01 13:13:00 +0000

    push into array render UI, state spread operator

--- src/App.js
+++ src/App.js
@@ -5,20 +5,33 @@ import './App.css';
 
 function App() {
   
- const [isGoingOut, setIsGoingOut] = React.useState(true);
+//  const [isGoingOut, setIsGoingOut] = React.useState(true);
 
- function handleFlip(){
-  setIsGoingOut(preState => !preState)
- }
- let answer = isGoingOut ? "Yes" : "No"
+//  function handleFlip(){
+//   setIsGoingOut(preState => !preState)
+//  }
+//  let answer = isGoingOut ? "Yes" : "No"
+
+  const [thingsArray, setThingsArray] = React.useState(['Thing 1', 'Thing 2'])
+
+  function addItem(){
+    setThingsArray(preState => [...preState, `Thing ${preState.length + 1}`])
+  }
+  const allThingsArray = thingsArray.map((things) => <p>{things}</p>)
+  
+  
   return (
     <div className='practise'>{/*Remember to change class*/}
       {/*<Navbar />
       <Meme />*/}
-      <h1 className="state--title">Do I feel like going out tonight?</h1>
+      {/* <h1 className="state--title">Do I feel like going out tonight?</h1>
       <diV className="state--value" onClick={handleFlip}>
           <h1>{answer}</h1>
-      </diV>
+      </diV> */}
+      <button onClick={addItem}>Add Item</button>
+      {allThingsArray}
+
+      
     </div>
   );
 }
